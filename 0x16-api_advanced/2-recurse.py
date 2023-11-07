cd28021b8247d9667edc9864df30af71fe492f06
Ahmedkel <ahmedkeloch@gmail.com>
2023-11-07 16:05:14 +0100

    list containing the titles of all hot articles

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+list containing the titles of
+all hot articles for a given subreddit.
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
+    headers = {"User-Agent": "custom"}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        return None
+
+    data = response.json()["data"]
+    hot_list.extend([post["data"]["title"] for post in data["children"]])
+
+    after = data["after"]
+
+    if after is None:
+        return hot_list
+    else:
+        return recurse(subreddit, hot_list, after)
